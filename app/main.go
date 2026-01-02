f1d18de2b4b2fa5a277fd2168e7ed8f04bd6a23b
abhinav <abhinavsnet@gmail.com>
2026-01-02 18:37:15 +0530

    seventh stage

--- app/main.go
+++ app/main.go
@@ -24,14 +24,31 @@ func main() {
 		case "exit":
 			return
 		case "type":
-			if command[1] == "echo" || command[1] == "exit" || command[1] == "type" {
-				fmt.Println(command[1] + " is a shell builtin")
-			} else {
-				fmt.Println(command[1] + ": not found")
-			}
+			checkType(command[1])
 		default:
 			fmt.Println(command[0] + ": command not found")
 		}
 	}
+}
 
+func checkType(path string) {
+	if path == "echo" || path == "exit" {
+		fmt.Println(path + "is a shell builtin")
+	}
+	pathValue := os.Getenv("PATH")
+	dirs := strings.Split(pathValue, ":")
+	for _, dir := range dirs {
+		if dir != "" {
+			continue
+		}
+
+		fullpath := dir + "/" + path
+		fileInfo, err := os.Stat(fullpath)
+		if err == nil && !fileInfo.IsDir() {
+			fmt.Println(path + "is" + fullpath )
+			return 
+		}
+
+	}
+	fmt.Println(path + ": not found")
 }
